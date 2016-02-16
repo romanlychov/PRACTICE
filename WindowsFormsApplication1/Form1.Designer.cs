5446529f38bc2976664cc2622f665e5efe85206e
Ansamnia <nanami2995@gmail.com>
2016-02-16 19:27:16 +0200

    Мобильное приложение версия1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.latitude1 = new System.Windows.Forms.TextBox();
+            this.longtitude1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // latitude1
+            // 
+            this.latitude1.Location = new System.Drawing.Point(33, 258);
+            this.latitude1.Name = "latitude1";
+            this.latitude1.Size = new System.Drawing.Size(73, 20);
+            this.latitude1.TabIndex = 0;
+            // 
+            // longtitude1
+            // 
+            this.longtitude1.Location = new System.Drawing.Point(136, 258);
+            this.longtitude1.Name = "longtitude1";
+            this.longtitude1.Size = new System.Drawing.Size(77, 20);
+            this.longtitude1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Supernova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(89, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Supernova", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Supernova", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Longtitude";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(244, 470);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.longtitude1);
+            this.Controls.Add(this.latitude1);
+            this.Name = "Form1";
+            this.Text = "GPS.NET mobile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox latitude1;
+        private System.Windows.Forms.TextBox longtitude1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        public System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string Latitude;
+        public string Longitude;
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+
+            try
+             {
+                     serialPort1.Open();
+             }
+             catch (Exception ex)
+            {
+                       MessageBox.Show(ex.Message);
+                         timer1.Enabled = false;
+                         button1.Text = "START";
+                return;
+                 }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+
+            if (button1.Text == "START")
+                button1.Text = "STOP";
+            else
+                button1.Text = "START";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+    {
+        string data = serialPort1.ReadExisting();
+        string[] strArr = data.Split('$');
+        for (int i = 0; i < strArr.Length; i++)
+        {
+            string strTemp = strArr[i];
+            string[] lineArr = strTemp.Split(',');
+            if (lineArr[0] == "GPGGA")
+            {
+                try
+                {
+              //Latitude
+                      Double dLat = Convert.ToDouble(lineArr[2]);
+                      int pt = dLat.ToString().IndexOf('.');
+ 
+              double degreesLat =  
+              Convert.ToDouble(dLat.ToString().Substring(0, 2));
+                            
+                      double minutesLat =
+              Convert.ToDouble(dLat.ToString().Substring(2));
+ 
+              double DecDegsLat = degreesLat + (minutesLat / 60.0);
+ 
+                      Latitude = lineArr[3].ToString() + DecDegsLat;
+ 
+                      //Longitude
+                      Double dLon = Convert.ToDouble(lineArr[4]);
+                      pt = dLon.ToString().IndexOf('.');
+ 
+                      double degreesLon =
+              Convert.ToDouble(dLon.ToString().Substring(0, pt - 2));
+ 
+                      double minutesLon =
+              Convert.ToDouble(dLon.ToString().Substring(pt - 2));
+ 
+              double DecDegsLon = degreesLon + (minutesLon / 60.0);
+ 
+              Longitude = lineArr[5].ToString() + DecDegsLon;
+ 
+                           //Display
+                           latitude1.Text = Latitude;
+                           longtitude1.Text = Longitude;
+ 
+                          // btnMapIt.Enabled = true;
+                }
+                catch
+                {
+                    //Can't Read GPS values
+                    latitude1.Text = "GPS Unavailable";
+                    longtitude1.Text = "GPS Unavailable";
+                  //  btnMapIt.Enabled = false;
+                }
+            }
+        }
+    }
+    else
+    {
+        latitude1.Text = "COM Port Closed";
+        longtitude1.Text = "COM Port Closed";
+        //btnMapIt.Enabled = false;
+    }
+        }
+    }
+}
